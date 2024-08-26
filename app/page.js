ed3a2f623020baee93893b945afc4755576a90e1
KiritMand <kiritmand@gmail.com>
2024-08-26 01:51:59 -0400

    frontend

--- app/page.js
+++ app/page.js
@@ -1,56 +1,24 @@
 'use client'
 import Image from "next/image";
 import {useState} from "react";
-import {Box, Stack, TextField, Button} from '@mui/material';
+import {Box, Stack, TextField, Button, Typography, createTheme, ThemeProvider} from '@mui/material';
+
+
 
 export default function Home() {
-  const [messages, setMessages] = useState([
-    {
-      role: "assistant",
-      content:"Hello, I'm the Rate My Professor Support Assistant. How can I help you today?"
-    }
-  ])
+  
+  const theme = createTheme({
+    typography: {
 
-  const [message, setMessage] = useState('')
-  const sendMessage = async ()=>{
-    setMessages((messages)=>[
-      ...messages,
-      {role: "user", content: message},
-      {role: "assistant", content: ''}
-    ])
-    
-    setMessage('')
-    const response = fetch('/api/chat', {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify([...messages, {role: "user", content:message}])
-    }).then(async(res) =>{
-      const reader = res.body.getReader()
-      const decoder = new TextDecoder()
+      fontWeightBold: 700,
+      fontWeightLight: 70,
 
-      let result = ''
-      return reader.read().then(function processText({done, value}){
-        if(done){
-          return result
-        }
-        const text = decoder.decode(value || new Uint8Array(), {stream: true})
-        setMessages((messages)=>{
-          let lastMessage = messages[messages.length - 1]
-          let otherMessages = messages.slice(0, messages.length - 1)
-          return [
-            ...otherMessages,
-             {...lastMessage, content: lastMessage.content + text},
-          ]
-        })
+    },
 
-        return reader.read().then(processText)
-      })
-    })
-    
-  }
+  });
+  
   return (
+    <ThemeProvider theme={theme}>
     <Box
       width="100vw"
       height="100vh"
@@ -58,57 +26,29 @@ export default function Home() {
       flexDirection="column"
       justifyContent="center"
       alignItems="center"
+      bgcolor="primary.main"
     >
       <Stack
         direction={'column'}
-        width="500px"
-        height="700px"
-        border="1px solid black"
+        width="50%"
+        height="90%"
+        border="1px secondary.contrastText"
         p={2}
         spacing={3}
-      >
-        <Stack
-          direction={'column'}
-          spacing={2}
-          flexGrow={1}
-          overflow="auto"
-          maxHeight="100%"
-        >
-          {messages.map((message, index) => (
-            <Box
-              key={index}
-              display="flex"
-              justifyContent={
-                message.role === 'assistant' ? 'flex-start' : 'flex-end'
-              }
-            >
-              <Box
-                bgcolor={
-                  message.role === 'assistant'
-                    ? 'primary.main'
-                    : 'secondary.main'
-                }
-                color="white"
-                borderRadius={16}
-                p={3}
-              >
-                {message.content}
-              </Box>
-            </Box>
-          ))}
-        </Stack>
-        <Stack direction={'row'} spacing={2}>
-          <TextField
-            label="Message"
-            fullWidth
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <Button variant="contained" onClick={sendMessage}>
-            Send
-          </Button>
-        </Stack>
+      > 
+        <Box>
+          <Box pb={8}><Typography variant="h1" color='white'  sx={{ fontWeight: 'fontWeightLight', flexGrow: 1, textAlign:'center'}} >
+          ProfPulse
+          </Typography></Box>
+          <Box bgcolor="primary.light" borderRadius={5} pt={4} pb={4} pl={3} pr={3}>
+            <Typography variant="h3" color='white' sx={{ flexGrow: 1, textAlign:'center', fontFamily: 'sans-serif'}}>Welcome to ProfPulse! </Typography>
+            <Typography variant="h6" color='white' sx={{ flexGrow: 1, textAlign:'center', fontFamily: 'sans-serif'}}>ProfPulse is the best rate my professor app for finding and rating university professors based on real student experiences. Discover the best instructors, share your insights, and make informed decisions about your courses—all in one easy-to-use platform!</Typography>
+          </Box> 
+        </Box> 
+        <Box pt={1} alignContent='center' alignItems='center' alignSelf='center' textAlign='center'><Button variant="contained" href="/result" color="secondary">Rate My Professor →</Button></Box>
       </Stack>
     </Box>
+    </ThemeProvider>
   )
 }
+
